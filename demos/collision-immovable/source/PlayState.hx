81b4a6e78a856deca8e622a635dae3818db63ea1
SeiferTim <seifertim@gmail.com>
2015-06-10 15:56:03 -0500

    Fixed immovable demo?

--- demos/collision-immovable/source/PlayState.hx
+++ demos/collision-immovable/source/PlayState.hx
@@ -1,12 +1,12 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-
 	private var spriteA:FlxSprite;
 	private var boxA:FlxSprite;
 	
@@ -28,20 +28,17 @@ class PlayState extends FlxState
 		boxB.y = FlxG.height * .75 - boxB.height / 2;
 		add(boxB);
 		
-		
 		spriteA = new FlxSprite(AssetPaths.sprite__png);
 		spriteA.x = 16;
 		spriteA.y = FlxG.height * .25 - spriteA.height / 2;
 		spriteA.velocity.x = 100;
 		add(spriteA);
 		
-		
 		spriteB = new FlxSprite(AssetPaths.sprite__png);
 		spriteB.x = 16;
 		spriteB.y = FlxG.height * .75 - spriteB.height / 2;
 		spriteB.velocity.x = 100;
 		add(spriteB);
-		
 		
 		boxB.immovable = true;
 	}
@@ -53,10 +50,41 @@ class PlayState extends FlxState
 		FlxG.collide(spriteA, boxA);
 		FlxG.collide(spriteB, boxB);
 		
-		if (spriteA.x >= FlxG.width)
+		if (spriteA.velocity.x > 0)
 		{
-			spriteA.x = spriteB.x = 16;
-			boxA.x = FlxG.width - boxA.width - 16;
+			if (spriteA.x >= FlxG.width - spriteA.width - 16)
+			{
+				spriteA.velocity.x = -150;
+				boxA.velocity.x = -100;
+			}
+			else
+			{
+				spriteA.velocity.x = 100;
+			}
 		}
+		else if (spriteA.x <= 16)
+		{
+			spriteA.x = 16;
+			spriteA.velocity.x = 100;
+		}
+		if (boxA.velocity.x < 0)
+		{
+			if (boxA.x <= FlxG.width - boxA.width - 16)
+			{
+				boxA.velocity.x = 0;
+				boxA.x = FlxG.width - boxA.width - 16;
+			}
+		}
+		
+		if (spriteB.justTouched(FlxObject.ANY))
+		{
+			spriteB.velocity.x = -150;
+		}
+		else if (spriteB.velocity.x < 0 && spriteB.x <= 16)
+		{
+			spriteB.velocity.x = 100;
+			spriteB.x = 16;
+		}
+		
 	}
 }
