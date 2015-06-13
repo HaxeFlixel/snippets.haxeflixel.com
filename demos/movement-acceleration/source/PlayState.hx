6754ea7d2d9a0dd1cc91410cb657782748c24fa1
Timothy Hely <seifertim@gmail.com>
2015-06-13 16:52:49 -0500

    changed and replaced movement-velocity and movement-acceleration demos

---
+++ demos/movement-acceleration/source/PlayState.hx
@@ -0,0 +1,40 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	private var sprite:FlxSprite;
+	
+	override public function create():Void
+	{
+		bgColor = 0;
+		super.create();
+		
+		sprite = new FlxSprite(AssetPaths.sprite__png);
+		sprite.y = FlxG.height / 2 - sprite.height / 2;
+		add(sprite);
+		
+		sprite.acceleration.x = 400;
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		if (sprite.velocity.x > 0 && sprite.x >= FlxG.width - sprite.width)
+		{
+			sprite.x = FlxG.width - sprite.width;
+			sprite.velocity.x = 0;
+			sprite.acceleration.x *= -1;
+		}
+		else if (sprite.velocity.x < 0 && sprite.x <= 0)
+		{
+			sprite.x = 0;
+			sprite.velocity.x = 0;
+			sprite.acceleration.x *= -1;
+		}
+		
+		super.update(elapsed);
+	}
+}
---
+++ demos/movement-velocity/source/PlayState.hx
@@ -0,0 +1,39 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	
+	private var sprite:FlxSprite;
+	
+	override public function create():Void
+	{
+		bgColor = 0;
+		super.create();
+		
+		sprite = new FlxSprite(AssetPaths.sprite__png);
+		sprite.y = FlxG.height / 2 - sprite.height / 2;
+		add(sprite);
+		
+		sprite.velocity.x = 100;
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		if (sprite.velocity.x > 0 && sprite.x >= FlxG.width - sprite.width)
+		{
+			sprite.x = FlxG.width - sprite.width;
+			sprite.velocity.x *= -1;
+		}
+		else if (sprite.velocity.x < 0 && sprite.x <= 0)
+		{
+			sprite.x = 0;
+			sprite.velocity.x *= -1;
+		}
+		
+		super.update(elapsed);
+	}
+}
