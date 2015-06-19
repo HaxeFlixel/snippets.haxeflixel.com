d611a5dad3ac62242ed672aade61b862890b163d
SeiferTim <seifertim@gmail.com>
2015-06-19 09:27:22 -0500

    more travis testing

--- build/install-haxe.sh
+++ build/install-haxe.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mkdir -p ~/neko
+curl -s -L --retry 3 'http://nekovm.org/_media/neko-2.0.0-linux64.tar.gz' | tar -C ~/neko -x -z --strip-components=1 -f -
+export NEKOPATH="${HOME}/neko"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${NEKOPATH}"
+export PATH="${PATH}:${NEKOPATH}"
 mkdir -p ~/haxe
 curl -s -L --retry 3 'http://haxe.org/website-content/downloads/3.2.0/downloads/haxe-3.2.0-linux64.tar.gz' | tar -C ~/haxe -x -z --strip-components=1 -f -
 export PATH="${PATH}:${HOME}/haxe"
