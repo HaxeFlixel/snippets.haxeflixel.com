482b36c3646d69d34b3408af5b4b5b93807527a5
SeiferTim <seifertim@gmail.com>
2015-06-19 10:26:51 -0500

    so close...

--- build/build-demos.sh
+++ build/build-demos.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ev
+export NEKOPATH="${HOME}/neko"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${NEKOPATH}"
+export PATH="${PATH}:${NEKOPATH}"
+export PATH="${PATH}:${HOME}/haxe"
+export HAXE_STD_PATH="${HOME}/haxe/std"
 for f in $(find ./demos/**/Project.xml -type f); do
   haxelib run lime build $f flash -release
 done
