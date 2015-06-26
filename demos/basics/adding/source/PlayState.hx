04067861337d716b9f4f80f5783f298deed0411b
Gama11 <jensfischer95@gmail.com>
2015-06-26 20:48:44 +0200

    Simpler adding demo source
    [skip ci]

--- demos/basics/adding/source/PlayState.hx
+++ demos/basics/adding/source/PlayState.hx
@@ -7,27 +7,18 @@ using flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
-
-	private var box:FlxSprite;
-	private var sprite:FlxSprite;
-	
 	override public function create():Void
 	{
 		bgColor = 0;
 		super.create();
 		
-		box = new FlxSprite(AssetPaths.bigbox__png);
-		box.screenCenter(true, true);
+		var box = new FlxSprite(AssetPaths.bigbox__png);
+		box.screenCenter();
 		
-		sprite = new FlxSprite(AssetPaths.sprite__png);
-		sprite.screenCenter(true, true);
+		var sprite = new FlxSprite(AssetPaths.sprite__png);
+		sprite.screenCenter();
 		
 		add(box);
 		add(sprite);
-	}
-
-	override public function update(elapsed:Float):Void
-	{
-		super.update(elapsed);
 	}
 }
