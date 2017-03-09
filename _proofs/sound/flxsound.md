e0ffff223c235d4cfb2a04177fd9dd55d6920f01
Seraj Nagi <Lucas_Naruto_AE@Yahoo.com>
2017-03-09 10:40:19 +0200

    Added FlxSound
    Feel free to replace the current sound if need be.
    (Points if you know where the current one is from)

---
+++ _proofs/sound/flxsound.md
@@ -0,0 +1,13 @@
+---
+title: FlxSound
+concept: Sound
+complexity: 1
+tags: [sound]
+order: 1
+---
+{% api flixel.FlxG %} provides a way of playing sounds, should you not feel like initiating a FlxSound instance.
+
+```haxe
+// Play a sound
+FlxG.sound.play();
+```
---
+++ demos/sound/flxsound/source/Main.hx
@@ -0,0 +1,17 @@
+package;
+
+import flixel.FlxGame;
+import openfl.Lib;
+import openfl.display.Sprite;
+import flixel.FlxG;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+
+		addChild(new FlxGame(320, 240, PlayState, 1, 60, 60, true, false));
+		FlxG.camera.pixelPerfectRender = false;
+	}
+}
