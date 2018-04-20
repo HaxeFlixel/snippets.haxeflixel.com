40388663424bdfa211a3ae73eb32b93fc999dae3
Jens Fischer <jensfischer95@gmail.com>
2018-04-20 17:14:01 +0200

    [Travis] update to Haxe 3.4.7 and Neko 2.1.0

--- build/haxe-setup.sh
+++ build/haxe-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-if [ ! -f ~/neko/neko-2.0.0-linux64.tar.gz ]; then
+if [ ! -f ~/neko/neko-2.1.0-linux64.tar.gz ]; then
   mkdir -p ~/neko
-  curl -s -L --retry 3 'http://nekovm.org/_media/neko-2.0.0-linux64.tar.gz' | tar -C ~/neko -x -z --strip-components=1 -f -
+  curl -s -L --retry 3 'http://nekovm.org/_media/neko-2.1.0-linux64.tar.gz' | tar -C ~/neko -x -z --strip-components=1 -f -
 fi
-if [ ! -f ~/haxe/haxe-3.2.0-linux64.tar.gz ]; then
+if [ ! -f ~/haxe/haxe-3.4.7-linux64.tar.gz ]; then
   mkdir -p ~/haxe
-  curl -s -L --retry 3 'http://haxe.org/website-content/downloads/3.2.0/downloads/haxe-3.2.0-linux64.tar.gz' | tar -C ~/haxe -x -z --strip-components=1 -f -
+  curl -s -L --retry 3 'http://haxe.org/website-content/downloads/3.4.7/downloads/haxe-3.4.7-linux64.tar.gz' | tar -C ~/haxe -x -z --strip-components=1 -f -
 fi
 mkdir -p ~/haxe/lib
 haxelib setup ~/haxe/lib
