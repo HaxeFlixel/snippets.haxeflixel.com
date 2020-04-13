358dfbdf486c2cd66d4932253493f35b861efda8
Fab <fabianjarrett@gmail.com>
2020-04-13 10:34:10 +0100

    Button demo (#17)
    Co-authored-by: Jens Fischer <jensfischer95@gmail.com>

---
+++ demos/input/button/source/PlayState.hx
@@ -0,0 +1,23 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+
+		var button = new FlxButton(0, 0, "Click me", onButtonClicked);
+		button.screenCenter();
+		add(button);
+	}
+
+	function onButtonClicked()
+	{
+		FlxG.camera.flash(FlxColor.WHITE, 0.33);
+	}
+}
