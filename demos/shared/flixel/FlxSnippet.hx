d680e938cd7c521d6f915664ca1484a166bd54ae
George Kurelic <Gkurelic@gmail.com>
2022-10-04 22:17:17 -0500

    conditional constructor (#34)

--- demos/shared/flixel/FlxSnippet.hx
+++ demos/shared/flixel/FlxSnippet.hx
@@ -9,7 +9,7 @@ class FlxSnippet extends FlxGame
 	public function new(gameWidth = 0, gameHeight = 0, initialState:Class<FlxState>,
 			updateFramerate = 60, drawFramerate = 60)
 	{
-		super(gameWidth, gameHeight, initialState, updateFramerate, drawFramerate);
+		super(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") 1.0, #end updateFramerate, drawFramerate, true);
 		FlxG.autoPause = false;
 	}
 }
