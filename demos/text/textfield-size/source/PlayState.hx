5fcf2e10caed7b908342a785241be7a63c09d01b
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-10-19 15:43:11 +1000

    Update text-size demo. It intended to be more simple, but appeared to have more code compare to tween version.

--- demos/text/textfield-size/source/PlayState.hx
+++ demos/text/textfield-size/source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.FlxG;
+import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 import flixel.FlxState;
@@ -8,43 +10,68 @@ class PlayState extends FlxState
 {
 	override public function create()
 	{
-		bgColor = 0;
 		super.create();
 
-		var fontSize = 16;
+		bgColor = 0;
 
-		var autoSizedText = new FlxText(0, 0, 0, "autosized", fontSize);
-		autoSizedText.screenCenter(X);
-		autoSizedText.y = 30;
-		colorize(autoSizedText);
-		add(autoSizedText);
+		var fontSize = 18;
+		var xCoord = 10;
+		var yCoord = 20;
+		var gap = 10;
 
-		var fixedSize = new FlxText();
-		fixedSize.fieldWidth = 120;
+		var autoSized = new FlxText(xCoord, yCoord, 0, "autosized", fontSize);
+		colorizeText(autoSized);
+		add(autoSized);
+
+		var fixedSize = new FlxText(xCoord);
+		fixedSize.fieldWidth = 140;
 		fixedSize.fieldHeight = 50;
 		fixedSize.text = "fixed size";
 		fixedSize.size = fontSize;
-		fixedSize.screenCenter(X);
-		fixedSize.y = autoSizedText.getScreenBounds().bottom + 10;
-		colorize(fixedSize);
+		fixedSize.y = autoSized.y + autoSized.height + gap;
+		colorizeText(fixedSize);
 		add(fixedSize);
 
-		var autoHeight = new FlxText();
-		autoHeight.fieldWidth = 100;
+		var autoHeight = new FlxText(xCoord);
+		autoHeight.fieldWidth = 150;
 		autoHeight.fieldHeight = 0;
 		autoHeight.text = 'fixed width\nauto height';
 		autoHeight.size = fontSize;
-		autoHeight.screenCenter(X);
-		autoHeight.y = fixedSize.getScreenBounds().bottom + 10;
-		colorize(autoHeight);
+		autoHeight.y = fixedSize.y + fixedSize.height + gap;
+		colorizeText(autoHeight);
 		add(autoHeight);
+
+		var sf = 1.5;
+		var btn = new FlxButton(0, 0, () ->
+		{
+			var addon = 'Description: ';
+			updateText(autoSized, addon);
+			updateText(fixedSize, addon);
+			updateText(autoHeight, addon);
+		});
+		btn.text = 'Update Text';
+		btn.scale.scale(sf);
+		btn.updateHitbox();
+		btn.label.scale.scale(sf);
+		btn.label.updateHitbox();
+		btn.x = FlxG.width - btn.width - 10;
+		btn.y = FlxG.height - btn.height - 10;
+		add(btn);
 	}
 
-	function colorize(text:FlxText)
+	function colorizeText(text:FlxText)
 	{
 		text.color = FlxColor.CYAN;
 		// accessing background through underlying openfl TextField object
 		text.textField.background = true;
 		text.textField.backgroundColor = 0x0C365F;
+	}
+
+	function updateText(textField:FlxText, addon:String)
+	{
+		if (textField.text.indexOf(addon) == -1)
+			textField.text = addon + textField.text;
+		else
+			textField.text = textField.text.split(addon)[1];
 	}
 }
