46066d202a529a2263aa1958f7c14b8fbebf9f48
George FunBook <gkurelic@gmail.com>
2024-04-25 18:11:16 -0500

    fix indent

--- demos/text/flxtext/source/PlayState.hx
+++ demos/text/flxtext/source/PlayState.hx
@@ -10,9 +10,9 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		bgColor = 0;
-
-super.create();
-
+		
+		super.create();
+		
 		var text = new FlxText();
 		text.text = "Hello, World!";
 		text.color = FlxColor.CYAN;
