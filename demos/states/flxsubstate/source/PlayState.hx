f621f49597b24b42543af34ad78ec1372fa11f99
TracedInPurple <74588450+TracedInPurple@users.noreply.github.com>
2025-09-06 15:57:36 +0200

    Prevent the sprite from going off screen
    Its just a simple visual yet neat thing to have :3

--- demos/states/flxsubstate/source/PlayState.hx
+++ demos/states/flxsubstate/source/PlayState.hx
@@ -46,11 +46,11 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 		
-		if (sprite.x < 0 || sprite.x > FlxG.width)
+		if (sprite.x < 0 || sprite.x > FlxG.width - sprite.width)
 		{
 			sprite.velocity.x *= -1;
 		}
-		if (sprite.y < 0 || sprite.y > FlxG.height)
+		if (sprite.y < 0 || sprite.y > FlxG.height - sprite.height)
 		{
 			sprite.velocity.y *= -1;
 		}
